e72f3496e0bd9456299477915476799c88d2939a
Jean de Lima Lopes <jean.lopes@xpi.com.br>
2021-07-14 12:29:27 -0300

    add dockerfile

--- app/example.py
+++ app/example.py
@@ -1,8 +1,8 @@
 from ariadne import ObjectType, QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
+#from ariadne.wsgi import GraphQL
 
-# Define types using Schema Definition Language (https://graphql.org/learn/schema/)
-# Wrapping string in gql function provides validation and better error traceback
+
 type_defs = gql("""
     type Query {
         people: [Person!]!
@@ -16,10 +16,8 @@ type_defs = gql("""
     }
 """)
 
-# Map resolver functions to Query fields using QueryType
 query = QueryType()
 
-# Resolvers are simple python functions
 @query.field("people")
 def resolve_people(*_):
     return [
@@ -28,15 +26,15 @@ def resolve_people(*_):
     ]
 
 
-# Map resolver functions to custom type fields using ObjectType
 person = ObjectType("Person")
 
 @person.field("fullName")
 def resolve_person_fullname(person, *_):
     return "%s %s" % (person["firstName"], person["lastName"])
 
-# Create executable GraphQL schema
 schema = make_executable_schema(type_defs, query, person)
 
-# Create an ASGI app using the schema, running in debug mode
 app = GraphQL(schema, debug=True)
+
+#In command line start server with command:
+#uvicorn example:app
---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM tiangolo/uvicorn-gunicorn-starlette:python3.8-slim
+
+# These are core dependencies and should be updated with care
+RUN pip3 install 'uvicorn==0.14.*' 'gunicorn==20.*' 'ariadne==0.13.*' 'graphqlclient==0.2.*' 'asgi-lifespan==1.0.1'
+
+COPY ./app /app
+COPY requirements.txt /
+
+RUN pip3 install -r /requirements.txt
+
+WORKDIR /
+EXPOSE 5000/tcp
+CMD uvicorn --reload --host 0.0.0.0 --port 5000 --log-level info --app-dir app example:app
